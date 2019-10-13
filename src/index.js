6394ca8e022b8b5234ec4460f29bb280995b3988
CuB21 <igor.yakubovsky@gmail.com>
2019-10-13 13:11:50 +0300

    feat: calculate weeks

--- src/index.js
+++ src/index.js
@@ -4,11 +4,45 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+
+
+
+module.exports = function getTimeForEducation (focus = 'family', knowsProgramming = true, config = {family: 4} ) {
+      if (focus === 'family' &&  knowsProgramming === true) {
+        return Math.ceil(800/config['family']);
+      }
+      if (focus === 'friends' && knowsProgramming === true) {
+        return Math.ceil(800/config['friends']);
+      }
+      if (focus === 'normal_life' && knowsProgramming ===  true) {
+        return Math.ceil(800/config['normal_life']);
+      }
+      if (focus === 'profession' && knowsProgramming ===  true) {
+        return Math.ceil(800/config['profession']);
+      }
+      if (focus ==='carrier' && knowsProgramming ===  true) {
+        return Math.ceil(800/config['carrier']);
+      }
+      if (focus ==='top_peformance' && knowsProgramming ===  true) {
+        return Math.ceil(800/config['top_peformance']);
+      }
+      if (focus === 'family' &&  knowsProgramming === false) {
+        return Math.ceil(1300/config['family']);
+      }
+      if (focus === 'friends' && knowsProgramming === false) {
+        return Math.ceil(1300/config['friends']);
+      }
+      if (focus === 'normal_life' && knowsProgramming ===  false) {
+        return Math.ceil(1300/config['normal_life']);
+      }
+      if (focus === 'profession' && knowsProgramming ===  false) {
+        return Math.ceil(1300/config['profession']);
+      }
+      if (focus ==='carrier' && knowsProgramming ===  false) {
+        return Math.ceil(1300/config['carrier']);
+      }
+      if (focus ==='top_peformance' && knowsProgramming ===  false) {
+        return Math.ceil(1300/config['top_peformance']);
+      }
+
   };
-  
